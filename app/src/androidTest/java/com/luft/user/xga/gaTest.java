0ef82eb30e148c770d41df04f9168a5dbedb9072
lüftreich <luftreich@gmail.com>
2015-03-07 18:57:19 +0800

    First commit

---
+++ app/src/androidTest/java/com/luft/user/xga/gaTest.java
@@ -0,0 +1,7 @@
+package com.luft.user.xga;
+
+import junit.framework.TestCase;
+
+public class gaTest extends TestCase {
+
+}
---
+++ app/src/main/java/com/luft/user/xga/ga.java
@@ -0,0 +1,67 @@
+package com.luft.user.xga;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class ga extends ActionBarActivity {
+
+    private static final String TAG = ga.class.getSimpleName();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ga);
+
+        ActivityTracker.sendView(this, TAG);
+
+        final Button ok = (Button)findViewById(R.id.button1);
+
+        ok.setOnClickListener(new OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+              //  mConfirm.run();
+            @Override
+            public void onClick(View arg0) {
+
+                TextView textview = (TextView)findViewById(R.id.txt1);
+                textview.setText("Start GA");
+                ActivityTracker.sendEvent(getApplicationContext(), ActivityTracker.CAT_PREF, "Orz", "", 0L);
+
+                //Thread.sleep(5)
+                textview.setText("<统计>");
+
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ga, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
